65ed4b814752a9ac6a562c79b7553219d614dc70
Humoyun Ahmedov <humoyun.com@gmail.com>
2020-08-30 22:48:43 +0900

    dist removed from git cache

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_PROD = void 0;
-exports.IS_PROD = process.env.environment === "production";
-//# sourceMappingURL=constants.js.map
